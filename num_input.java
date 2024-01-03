543907bd0a3fe29172b8bd7e85e4850d1698c022
proGabby <inimfonwillie@gmail.com>
2024-01-03 23:07:58 +0100

    feat: receiving input with dialog using java

--- num_input.java
+++ num_input.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
 
+import javax.swing.JOptionPane;
+
 class NumInput {
     public static void main(String[] arg) {
+        getAgeAndNameViaDialog();
+    }
+
+    public static void getAgeInputViaCLI() {
         int age;
         System.out.println("please input your age: ");
         Scanner inputDevice = new Scanner(System.in);
@@ -10,5 +16,16 @@ class NumInput {
         System.out.println("please enter your birth month: ");
         String birthMonth = inputDevice.nextLine();
         System.out.println(" your age is " + age + ", your birth month is " + birthMonth);
+    }
+
+    public static void getAgeAndNameViaDialog() {
+        int age;
+        String name;
+        final String ageString = JOptionPane.showInputDialog(null, "Enter your age", "Age Dialog",
+                JOptionPane.INFORMATION_MESSAGE);
+        age = Integer.parseInt(ageString);
+        name = JOptionPane.showInputDialog(null, "Enter your fullName");
+
+        JOptionPane.showMessageDialog(null, "Hello " + name + ". You're " + age + " years old");
     }
 }
