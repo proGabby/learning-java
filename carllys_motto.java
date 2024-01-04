ac21add59b842b190dbda08220581f55153043f0
proGabby <inimfonwillie@gmail.com>
2024-01-04 21:18:49 +0100

    feat: carlys_cattering_program

---
+++ carllys_motto.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class CarlysCattering {
+
+    public static void main(String[] arg) {
+    
+     
+
+    }
+
+    public static void displayCarlyMotto() {
+        System.out.println("Carly’s makes the food that makes it a party.");
+    }
+
+    public static void displayCarlyMottoWithAsterisks() {
+        System.out.println("**************************************************");
+        System.out.println("* Carly's makes the food that makes it a party.  *");
+        System.out.println("**************************************************");
+    }
+
+   
+}
---
+++ sammy_motto.jav
@@ -0,0 +1,5 @@
+class SammySeashore{
+    public static void main(String[] arg){
+        
+    }
+}
